14fe15f1e624295aa51737f82071ae0560bf7013
SMakaiTakori <makait87@icloud.com>
2020-07-30 19:39:28 -0400

    added js files, created constructor functions for pet and activity js files

---
+++ front_end/js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    debugger;
+
+
+// read - fetch pets index
+
+// create - create a new pet
+
+//edit  - edit pet info
+
+})
---
+++ front_end/js/pets.js
@@ -0,0 +1,10 @@
+class Pet {
+    
+    constructor(name, owner, animal_type, mood, phrase){
+        this.name = name;
+        this.owner = owner;
+        this.animal_type = animal_type;
+        this.mood = mood;
+        this.phrase = phrase;
+    }
+}
