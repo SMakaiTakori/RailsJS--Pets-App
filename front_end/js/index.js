b36c8f0b9fe4f89afc887c2b68534589a431717a
SMakaiTakori <makait87@icloud.com>
2020-08-01 17:46:30 -0400

    created petForm function and added css and html

--- front_end/js/index.js
+++ front_end/js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchPets()
+    createForm()
+
     
 })
 
@@ -18,8 +20,32 @@ function fetchPets(){
     })
 }
 
+// create a form for user to input pet info for new pet
 
+function createForm(){
+    let petForm = document.getElementById('create-pet')
+    // console.log(petForm)
+    petForm.innerHTML += 
+    `
+    <form class="form-inline">
+    <div class="form-group mx-sm-3 mb-2">
+      <label for="name" class="sr-only">Pet Name</label>
+      <input type="name" class="form-control" id="name" placeholder="Enter Pet Name">
+    </div>
+    <div class="form-group mx-sm-3 mb-2">
+      <label for="owner" class="sr-only">Owner Name</label>
+      <input type="owner" class="form-control" id="owner" placeholder="Enter Owner's Name">
+    </div>
+    <select style='width:150px;' class="custom-select custom-select-sm mx-sm-3 mb-2">
+        <option selected>Select Pet Type</option>
+        <option value="1">Dog</option>
+        <option value="2">Lizard</option>
+        <option value="3">Dragon</option>
+    </select>
+    <button type="submit" class="btn btn-primary mb-2 mb-3">Create Pet!</button>
+  </form>
+    `
+}
 
-// create - create a new pet
 
 //edit  - edit pet info
