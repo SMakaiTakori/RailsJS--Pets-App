e7db519590fbb336772cae18da41af7094f767c9
SMakaiTakori <makait87@icloud.com>
2020-08-02 18:12:24 -0400

    added if statement to displayPet() and updated renderPet()

--- front_end/js/pets.js
+++ front_end/js/pets.js
@@ -13,6 +13,7 @@ class Pet {
     renderPet(){
         let petInfo = document.getElementById('pet-info')
         // petInfo.empty();
+        petInfo.innerHTML = " "
         petInfo.innerHTML +=
         `
         <ul>
@@ -28,16 +29,26 @@ class Pet {
         let petImage = document.getElementById('pet-image')
         let petData = document.getElementById('pet-info')
         let animal_type = document.getElementById('options').value; 
+        
         let dragon = "../front_end/css/images/cutestDragon.png"; 
         let wolf = "../front_end/css/images/wolf.jpg"
+        let panther = "../front_end/css/images/wolf.jpg"
+        let lizard = "../front_end/css/images/wolf.jpg"
 
-        petImage.src = dragon;
+        // console.log(animal_type)
+        // can be converted to a switch statement
 
-            // if (animal_type === 'Dragon'){     
-
-            //     src.appendChild(div);  
-            //     // console.log(src)
-            // }
+            if (animal_type === 'Dragon'){     
+                petImage.src = dragon;
+            } else if (animal_type === 'Wolf'){
+                petImage.src = wolf;
+            } else if (animal_type === 'Panther'){
+                petImage.src = panther;
+            } else if (animal_type === 'Lizard'){
+                petImage.src = lizard;
+            } else {
+                petImage.src = "";
+            }
     }  
    
 }
